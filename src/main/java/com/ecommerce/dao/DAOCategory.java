d862b7088a736053569dbf743d744474ccc3aa5b
Samarth Deelip Kalegaonkar <kalegaonkarsamarth@gmail.com>
2025-08-31 02:15:30 +0530

    Added DAO for Category to Operate Category Data

---
+++ src/main/java/com/ecommerce/dao/DAOCategory.java
@@ -0,0 +1,60 @@
+package com.ecommerce.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.ecommerce.entity.Category;
+
+public class DAOCategory {
+    private SessionFactory sessionFactory;
+
+    public DAOCategory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public void saveCategory(Category category) {
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            // session.save(category);
+            session.persist(category);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public Category getCategoryById(int id) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(Category.class, id);
+        }
+    }
+
+    public void updateCategory(Category category) {
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            // session.update(category);
+            session.merge(category);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteCategory(Category category) {
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            // session.delete(category);
+            session.remove(category);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        }
+    }
+}
