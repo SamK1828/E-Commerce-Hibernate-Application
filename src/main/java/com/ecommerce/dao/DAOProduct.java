e20840ac44ba1c2e5b6ab583e4c6218d99c8e0af
Samarth Deelip Kalegaonkar <kalegaonkarsamarth@gmail.com>
2025-09-01 01:48:03 +0530

    Created DAO For Product Entity

---
+++ src/main/java/com/ecommerce/dao/DAOProduct.java
@@ -0,0 +1,63 @@
+package com.ecommerce.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.ecommerce.entity.Product;
+
+public class DAOProduct {
+    private SessionFactory sessionFactory;
+
+    public DAOProduct(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public void saveProduct(Product product) {
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            // session.save(product);
+            session.persist(product);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null)
+                tx.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public Product getProductById(int id) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(Product.class, id);
+        }
+    }
+
+    public void updateProduct(Product product) {
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            // session.update(product);
+            session.merge(product);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null)
+                tx.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteProduct(Product product) {
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            // session.delete(product);
+            session.remove(product);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null)
+                tx.rollback();
+            e.printStackTrace();
+        }
+    }
+}
