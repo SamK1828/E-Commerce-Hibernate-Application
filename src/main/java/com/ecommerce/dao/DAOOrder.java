e7d607d61eeec1a1d0b97997411e3d46894e1ad5
Samarth Deelip Kalegaonkar <kalegaonkarsamarth@gmail.com>
2025-09-01 01:48:22 +0530

    Created DAO for Order Entity

---
+++ src/main/java/com/ecommerce/dao/DAOOrder.java
@@ -0,0 +1,60 @@
+package com.ecommerce.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.ecommerce.entity.Order;
+
+public class DAOOrder {
+    private SessionFactory sessionFactory;
+
+    public DAOOrder(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public void saveOrder(Order order) {
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            // session.save(order);
+            session.persist(order);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public Order getOrderById(int id) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(Order.class, id);
+        }
+    }
+
+    public void updateOrder(Order order) {
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            // session.update(order);
+            session.merge(order);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteOrder(Order order) {
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            // session.delete(order);
+            session.remove(order);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        }
+    }
+}
