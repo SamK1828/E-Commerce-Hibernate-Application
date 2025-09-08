d9e966ebeea42c954ed9d998369b3f5ae617e7e1
Samarth Deelip Kalegaonkar <kalegaonkarsamarth@gmail.com>
2025-09-09 00:00:49 +0530

    Created User Controller to Manage User Entity endpoints

---
+++ src/main/java/com/ecommerce/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.ecommerce.controller;
+
+import com.ecommerce.entity.User;
+import com.ecommerce.service.UserService;
+import com.ecommerce.service.impl.UserServiceImpl;
+import com.ecommerce.util.UtilHib;
+
+public class UserController {
+
+    // private static UserService userService = new UserServiceImpl(UtilHib.getFactory());
+    
+
+    public static void main(String[] args) {
+        // // Create a new user 
+        // User newUser = new User();
+        // newUser.setUsername("Samarth");
+        // newUser.setEmail("samarth@example.com");
+        // newUser.setPassword("12345");
+        // User savedUser = userService.createUser(newUser);
+        // System.out.println("User created: " + savedUser.getUsername());
+        // // Fetch all users
+        // System.out.println("\nFetching all users from DB:");
+        // for (User u : userService.getAllUsers()) {
+        //     System.out.println(u.getId() + " | " + u.getUsername() + " | " + u.getEmail());
+        // }
+        
+    }
+
+}
