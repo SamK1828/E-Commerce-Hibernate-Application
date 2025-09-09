0287f0cb4214cfa4f2a142994fdc5a25d4556517
Samarth Deelip Kalegaonkar <kalegaonkarsamarth@gmail.com>
2025-09-09 18:24:04 +0530

    Minor changes woth User Controller

--- src/main/java/com/ecommerce/controller/UserController.java
+++ src/main/java/com/ecommerce/controller/UserController.java
@@ -13,21 +13,20 @@ public class UserController {
     // private static UserService userService = new UserServiceImpl(UtilHib.getFactory());
     
 
-    public static void main(String[] args) {
-        // // Create a new user 
-        // User newUser = new User();
-        // newUser.setUsername("Samarth");
-        // newUser.setEmail("samarth@example.com");
-        // newUser.setPassword("12345");
-        // User savedUser = userService.createUser(newUser);
-        // System.out.println("User created: " + savedUser.getUsername());
-        // // Fetch all users
-        // System.out.println("\nFetching all users from DB:");
-        // for (User u : userService.getAllUsers()) {
-        //     System.out.println(u.getId() + " | " + u.getUsername() + " | " + u.getEmail());
-        // }
-           
-    }
+    // public static void main(String[] args) {
+    //     // // Create a new user 
+    //     // User newUser = new User();
+    //     // newUser.setUsername("Samarth");
+    //     // newUser.setEmail("samarth@example.com");
+    //     // newUser.setPassword("12345");
+    //     // User savedUser = userService.createUser(newUser);
+    //     // System.out.println("User created: " + savedUser.getUsername());
+    //     // // Fetch all users
+    //     // System.out.println("\nFetching all users from DB:");
+    //     // for (User u : userService.getAllUsers()) {
+    //     //     System.out.println(u.getId() + " | " + u.getUsername() + " | " + u.getEmail());
+    //     // }
+    // }
 
      private static final Scanner sc = new Scanner(System.in);
     private static final UserService userService = new UserServiceImpl(UtilHib.getFactory());
