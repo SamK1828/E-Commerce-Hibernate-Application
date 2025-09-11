a9e598905005a27e36655f8092c711da7f76bfe0
Samarth Deelip Kalegaonkar <kalegaonkarsamarth@gmail.com>
2025-09-11 23:42:47 +0530

    modified address service implementation

--- src/main/java/com/ecommerce/service/impl/AddressServiceImpl.java
+++ src/main/java/com/ecommerce/service/impl/AddressServiceImpl.java
@@ -7,42 +7,41 @@ import org.hibernate.SessionFactory;
 import com.ecommerce.dao.DAOAddress;
 import com.ecommerce.entity.Address;
 import com.ecommerce.service.AddressService;
-import com.mysql.cj.Session;
 
 public class AddressServiceImpl implements AddressService {
-       private final DAOAddress addressDao;
+    private final DAOAddress addressDAO;
 
     public AddressServiceImpl(SessionFactory factory) {
-        this.addressDao = new DAOAddress(factory);
+        this.addressDAO = new DAOAddress(factory);
     }
 
     @Override
     public Address createAddress(Address address) {
-        addressDao.saveAddress(address);
+        addressDAO.saveAddress(address);
         return address;
     }
 
     @Override
     public Address getAddressById(Long id) {
-        return addressDao.getAddressById(id.intValue());
+        return addressDAO.getAddressById(id);
     }
 
     @Override
     public List<Address> getAllAddresses() {
-        return addressDao.getAllAddresses();
+        return addressDAO.getAllAddresses();
     }
 
     @Override
     public Address updateAddress(Address address) {
-        addressDao.updateAddress(address);
+        addressDAO.updateAddress(address);
         return address;
     }
 
     @Override
     public boolean deleteAddress(Long id) {
-        Address address = addressDao.getAddressById(id.intValue());
+        Address address = addressDAO.getAddressById(id);
         if (address != null) {
-            addressDao.deleteAddress(address);
+            addressDAO.deleteAddress(address);
             return true;
         }
         return false;
